bf66e34ab315a979fefb2120afe98d2b09525d31
Jordann Racine <jordann.racine@etu.cegepdrummond.ca>
2025-02-12 11:11:39 -0500

    WIP: Création tests de combat

--- tests/tuxemon/test_combat.py
+++ tests/tuxemon/test_combat.py
@@ -2,29 +2,23 @@
 # Copyright (c) 2014-2025 William Edwards <shadowapex@gmail.com>, Benjamin Bean <superman2k5@gmail.com>
 import unittest
 from unittest import mock
-from tuxemon.battle import Battle
+from tuxemon.combat  import fainted
 from tuxemon.monster import Monster
 
 class TestCombat(unittest.TestCase):
 
     def setUp(self):
-        self.battle = Battle()
-        self.tuxemon = Monster(name="Testmon", max_hp=10, attack=5, defense=3)
-        self.opponent = Monster(name="Foe", max_hp=10, attack=5, defense=3)
+        self.tuxemon = Monster()
+        self.opponent = Monster()
 
-    def test_tuxemon_faints_during_battle(self):
-        """Test that a Tuxemon properly faints when HP reaches 0"""
-        # Deal damage greater than its current HP
-        self.tuxemon.take_damage(15)
+    def test_tuxemon_faints_during_combat(self):
 
-        # Check if HP is actually 0
+        self.tuxemon.faint()
+
         self.assertEqual(self.tuxemon.current_hp, 0)
 
-        # Check if the built-in function recognizes it as fainted
-        self.assertTrue(self.tuxemon.is_fainted())
+        self.assertTrue(self.tuxemon.hp == 0)
 
-        # Ensure the battle system reacts properly
-        self.battle.handle_fainted(self.tuxemon)
+        #self.combat.fainted(self.tuxemon)
 
-        # (Optional) If your system removes fainted Tuxemon from the active battle
-        self.assertNotIn(self.tuxemon, self.battle.active_tuxemon)
+        #self.assertNotIn(self.tuxemon, self.combat.active_tuxemon)
