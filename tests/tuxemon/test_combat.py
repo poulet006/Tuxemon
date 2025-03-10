9894906140baf4c5dfe225cb5610ec11781445ec
Jordann Racine <jordann.racine@etu.cegepdrummond.ca>
2025-03-09 23:48:46 -0400

    cleanup of class test_combat

--- tests/tuxemon/test_combat.py
+++ tests/tuxemon/test_combat.py
@@ -1,46 +1,49 @@
 # SPDX-License-Identifier: GPL-3.0
 # Copyright (c) 2014-2025 William Edwards <shadowapex@gmail.com>, Benjamin Bean <superman2k5@gmail.com>
 import unittest
-import pygame
-from unittest.mock import MagicMock
+from unittest import mock
+from tuxemon.db import BattleGraphicsModel
 from tuxemon.states.combat.combat import CombatState
-from tuxemon.combat import fainted
 from tuxemon.monster import Monster
-from tuxemon.player import Player
 from tuxemon.npc import NPC
-from tuxemon.states.world.worldstate import WorldState
 
 class TestCombat(unittest.TestCase):
 
-    def setUp(self):
-        # Mock dependencies for CombatState
-        self.mock_graphics = MagicMock(name="mock_graphics")
-        self.mock_player = MagicMock(spec=NPC, name="Player")
-        self.mock_npc = MagicMock(spec=NPC, name="NPC")
-        self.mock_monster = MagicMock(spec=Monster, name="Monster")
+    def mockNPC(self) -> None:
+        self.monsters = []
+        self.isplayer = False
+        self.game_variables = {}
 
-        # Set up mock monster properties
+    def mockPlayer(self) -> None:
+        self.monsters = []
+        self.isplayer = True
+        self.game_variables = {}
+
+    def seup(self):
+        with mock.patch.object(NPC, "__init__", self.mockPlayer()):
+            self.mock_player = NPC()
+        with mock.patch.object(NPC, "__init__", self.mockNPC()):
+            self.mock_npc = NPC()
+        self.mock_monster = Monster()
+        self.mock_graphics = BattleGraphicsModel()
         self.mock_monster.hp = 50
         self.mock_monster.name = "TestMonster"
+        self.mock_player.add_monster(self.mock_monster)
 
-        # Create instance of CombatState
         self.combat_state = CombatState(
             players=(self.mock_player, self.mock_npc),
-            graphics=None,  # Graphics is no longer needed for this test
+            graphics=self.mock_graphics,
             combat_type="trainer",
         )
+        self.combat_state.isplayer = self.isplayer = True
 
     def test_combat_state_initialization(self):
-        """Test that the CombatState initializes properly."""
         self.assertEqual(len(self.combat_state.players), 2)
-        self.assertFalse(self.combat_state.is_trainer_battle is None)
         self.assertTrue(self.combat_state.is_trainer_battle)
 
     def test_add_monster_to_battle(self):
-        """Test adding a monster to the battle."""
         self.combat_state.monsters_in_play[self.mock_player].append(self.mock_monster)
 
-        # Assertions
         self.assertIn(self.mock_monster, self.combat_state.monsters_in_play[self.mock_player])
         self.assertEqual(
             self.combat_state.monsters_in_play[self.mock_player][0].name,
@@ -48,17 +51,9 @@ class TestCombat(unittest.TestCase):
         )
 
     def test_monster_faint(self):
-        """Test that fainting a monster updates correctly."""
-        self.mock_monster.hp = 0  # Simulate the HP dropping to 0
+        self.mock_monster.hp = 0
         self.assertEqual(self.mock_monster.hp, 0)
-
-        # Simulate fainting behavior
         self.combat_state.monsters_in_play[self.mock_player].append(self.mock_monster)
         self.combat_state.monsters_in_play[self.mock_player].remove(self.mock_monster)
-
-        # Assertions
         self.assertNotIn(self.mock_monster, self.combat_state.monsters_in_play[self.mock_player])
 
-    def tearDown(self):
-        """Clean up after tests."""
-        self.combat_state = None
