5516a7f971decf20a981ae2274f47551109c64d4
Jordann Racine <jordann.racine@etu.cegepdrummond.ca>
2025-03-10 22:26:44 -0400

    wip: correction of a bug

--- tests/tuxemon/test_combat.py
+++ tests/tuxemon/test_combat.py
@@ -22,15 +22,16 @@ class TestCombat(unittest.TestCase):
         self.isplayer = True
         self.game_variables = {}
 
-    def seup(self):
+    def setup(self):
         with mock.patch.object(NPC, "__init__", self.mockPlayer()):
             self.mock_player = NPC()
         with mock.patch.object(NPC, "__init__", self.mockNPC()):
             self.mock_npc = NPC()
         self.mock_monster = Monster()
+        self.mock_monster2 = Monster()
         self.mock_graphics = BattleGraphicsModel()
         self.mock_monster.hp = 50
-        self.mock_monster.name = "TestMonster"
+        self.mock_monster2.name = "TestMonster"
         self.mock_player.add_monster(self.mock_monster)
 
         self.combat_state = CombatState(
@@ -38,16 +39,15 @@ class TestCombat(unittest.TestCase):
             graphics=self.mock_graphics,
             combat_type="trainer",
         )
-        self.combat_state.isplayer = self.isplayer = True
 
     def test_combat_state_initialization(self):
         self.assertEqual(len(self.combat_state.players), 2)
         self.assertTrue(self.combat_state.is_trainer_battle)
 
     def test_add_monster_to_battle(self):
-        self.combat_state.monsters_in_play[self.mock_player].append(self.mock_monster)
+        self.combat_state.monsters_in_play[self.mock_player].append(self.mock_monster2)
 
-        self.assertIn(self.mock_monster, self.combat_state.monsters_in_play[self.mock_player])
+        self.assertIn(self.mock_monster2, self.combat_state.monsters_in_play[self.mock_player])
         self.assertEqual(
             self.combat_state.monsters_in_play[self.mock_player][0].name,
             "TestMonster"
@@ -56,13 +56,4 @@ class TestCombat(unittest.TestCase):
     def test_monster_faint(self):
         self.mock_monster.hp = 0
         self.assertEqual(self.mock_monster.hp, 0)
-        self.combat_state.monsters_in_play[self.mock_player].append(self.mock_monster)
-        self.combat_state.monsters_in_play[self.mock_player].remove(self.mock_monster)
         self.assertNotIn(self.mock_monster, self.combat_state.monsters_in_play[self.mock_player])
-
-    def test_tuxemon_faints_during_battle(self):
-        self.tuxemon.take_damage(15)
-        self.assertEqual(self.tuxemon.current_hp, 0)
-        self.assertTrue(self.tuxemon.is_fainted())
-        self.battle.handle_fainted(self.tuxemon)
-        self.assertNotIn(self.tuxemon, self.battle.active_tuxemon)
